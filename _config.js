afdbce16bf3e81cd8d2273639d4d6f6c8cb05baf
Kevin Rodrigues <35588005+KevinRodrigues05@users.noreply.github.com>
2019-05-07 08:22:31 +0530

    Update _config.js

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'my-precious',
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/mean-social-token-auth',
   SALT_WORK_FACTOR: 10,
-  GITHUB_SECRET: process.env.GITHUB_SECRET || '9269f128a4f9cc769fa663e87ab975134dd78de7',
-  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'glFAzuaB-cCJc4HVUdZw-3M6',
-  INSTAGRAM_SECRET: process.env.INSTAGRAM_SECRET || '333ee37dce0644a8be3af4179b07f0f0'
+  GITHUB_SECRET: process.env.GITHUB_SECRET || '',
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || '',
+  INSTAGRAM_SECRET: process.env.INSTAGRAM_SECRET || ''
 };
